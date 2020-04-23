7cd1dbeeeadfefd9482e904e070032ebe9ab1a1e
mariusjagminas <mariusjagminas.it@gmail.com>
2020-04-23 19:18:30 +0200

    Updates

---
+++ main.69a4c26e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".hamburger"),n=document.querySelector(".navigation"),t=document.querySelectorAll(".navigation__link");e.addEventListener("click",(function(){n.classList.toggle("navigation--active")})),t.forEach((function(e){return e.addEventListener("click",(function(){n.classList.remove("navigation--active")}))}))}();
+//# sourceMappingURL=main.69a4c26e.js.map
