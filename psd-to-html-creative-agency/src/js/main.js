0c0b294d62db57e2ec8872ddc59d112da74f1050
mariusjagminas <mariusjagminas.it@gmail.com>
2020-04-28 21:54:12 +0200

    Add mobile navigation support for IE

--- psd-to-html-creative-agency/src/js/main.js
+++ psd-to-html-creative-agency/src/js/main.js
@@ -1,11 +1,15 @@
 const btn = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navigation");
-const navLinks = document.querySelectorAll(".navigation__link");
+const navLinkNodes = document.querySelectorAll(".navigation__link");
+
+const navLinks = Array.prototype.slice.call(navLinkNodes); // for IE support
 
 btn.addEventListener("click", () => {
-  navigation.classList.toggle("navigation--active")
+  navigation.classList.toggle("navigation--active");
 });
 
-navLinks.forEach(item => item.addEventListener("click", () => {
-  navigation.classList.remove("navigation--active")
-}))
+navLinks.forEach((item) =>
+  item.addEventListener("click", () => {
+    navigation.classList.remove("navigation--active");
+  })
+);
