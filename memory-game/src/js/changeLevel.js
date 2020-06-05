1260c3520366f3c1755a6564026cc6b92f88c749
mariusjagminas <mariusjagminas.it@gmail.com>
2020-06-05 15:28:34 +0200

    Implement redux

--- memory-game/src/js/changeLevel.js
+++ memory-game/src/js/changeLevel.js
@@ -1,30 +1,35 @@
-import global from './globalVariables';
+import store from './store/store';
+import { CHANGE_LEVEL } from './store/actions';
 import initGame from './initGame';
 
 function changeLevel(e) {
   switch (e.target.value) {
     case 'easy':
-      global.level = {
+      store.dispatch({
+        type: CHANGE_LEVEL,
         numberOfCards: 16,
         numberOfColumns: 'grid-template-columns: repeat(4, 1fr);',
-      };
+      });
       initGame();
       break;
     case 'medium':
-      global.level = {
+      store.dispatch({
+        type: CHANGE_LEVEL,
         numberOfCards: 36,
         numberOfColumns: 'grid-template-columns: repeat(6, 1fr);',
-      };
+      });
       initGame();
       break;
     case 'pro':
-      global.level = {
+      store.dispatch({
+        type: CHANGE_LEVEL,
         numberOfCards: 64,
         numberOfColumns: 'grid-template-columns: repeat(8, 1fr);',
-      };
+      });
       initGame();
       break;
     default:
+      initGame();
   }
 }
 
---
+++ memory-game/src/js/store/actions.js
@@ -0,0 +1,7 @@
+export const CHANGE_LEVEL = 'CHANGE_LEVEL';
+export const CLEAR_TIMER = 'CLEAR_TIMER';
+export const SET_TIMER = 'SET_TIMER';
+export const ADD_CARDS = 'ADD_CARDS';
+export const DELETE_CARDS = 'DELETE_CARDS';
+export const PUSH_CARD_TO_CARDS_PAIR = 'PUSH_CARD_TO_CARDS_PAIR';
+export const FLUSH_CARDS_PAIR = 'FLUSH_CARDS_PAIR';
